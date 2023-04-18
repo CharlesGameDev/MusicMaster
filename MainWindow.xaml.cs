f9866e6c016fcf5d5d416db779ebd50c1ced5faa
CharlesGameDev <99226468+CharlesGameDev@users.noreply.github.com>
2023-04-17 20:35:08 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Diagnostics;
+
+namespace MusicMaster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Play button clicked");
+        }
+    }
+}
